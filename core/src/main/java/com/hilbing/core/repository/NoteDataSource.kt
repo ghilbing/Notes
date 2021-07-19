c0ad02815945856f25b80f0671a3b64161b9e6cc
ghilbing <ghilbing@gmail.com>
2021-07-19 09:55:29 -0700

    Repository Implementation

---
+++ core/src/main/java/com/hilbing/core/repository/NoteDataSource.kt
@@ -0,0 +1,11 @@
+package com.hilbing.core.repository
+
+import com.hilbing.core.data.Note
+
+interface NoteDataSource {
+    suspend fun add(note: Note)
+    suspend fun get(id: Long): Note?
+    suspend fun getAll(): List<Note>
+    suspend fun remove(note: Note)
+
+}
