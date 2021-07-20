f643ce54b930615f6e7e1576a13e06e8fa52963d
ghilbing <ghilbing@gmail.com>
2021-07-19 18:23:46 -0700

    Framework - Use Cases

---
+++ app/src/main/java/com/hilbing/notes/framework/UseCases.kt
@@ -0,0 +1,13 @@
+package com.hilbing.notes.framework
+
+import com.hilbing.core.usecase.AddNote
+import com.hilbing.core.usecase.GetAllNotes
+import com.hilbing.core.usecase.GetNote
+import com.hilbing.core.usecase.RemoveNote
+
+data class UseCases (
+    val addNote: AddNote,
+    val getAllNotes: GetAllNotes,
+    val getNote: GetNote,
+    val removeNote: RemoveNote
+        )
