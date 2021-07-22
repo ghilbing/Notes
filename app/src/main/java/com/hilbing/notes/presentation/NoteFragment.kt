4d752482b389b76a9bffdcafd3f27b4274e1588c
ghilbing <ghilbing@gmail.com>
2021-07-21 18:10:55 -0700

    Framework - NoteFragment implementation

--- app/src/main/java/com/hilbing/notes/presentation/NoteFragment.kt
+++ app/src/main/java/com/hilbing/notes/presentation/NoteFragment.kt
@@ -1,17 +1,26 @@
 package com.hilbing.notes.presentation
 
+import android.content.Context.INPUT_METHOD_SERVICE
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation
+import com.hilbing.core.data.Note
 import com.hilbing.notes.R
+import com.hilbing.notes.framework.NoteViewModel
 import kotlinx.android.synthetic.main.fragment_note.*
 
 
 class NoteFragment : Fragment() {
 
+    private lateinit var viewModel : NoteViewModel
+    private var currentNote = Note("", "", 0L, 0L)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,9 +31,42 @@ class NoteFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(NoteViewModel::class.java)
+
         fabCheck.setOnClickListener {
-            Navigation.findNavController(it).popBackStack()
+            if(titleView.text.toString() != "" || contentView.text.toString() != ""){
+                val time = System.currentTimeMillis()
+                currentNote.title = titleView.text.toString()
+                currentNote.content = contentView.text.toString()
+                currentNote.updateTime = time
+                if(currentNote.id == 0L){
+                    currentNote.creationTime = time
+                }
+                viewModel.saveNote(currentNote)
+            } else {
+                Navigation.findNavController(it).popBackStack()
+            }
+
         }
+        
+        observeViewModel()
+    }
+    
+    private fun observeViewModel(){
+        viewModel.saved.observe(viewLifecycleOwner, Observer {
+            if(it){
+                Toast.makeText(context, "Done!", Toast.LENGTH_SHORT).show()
+                hideKeyboard()
+                Navigation.findNavController(titleView).popBackStack()
+            } else {
+                Toast.makeText(context, "Error. Try again!", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    private fun hideKeyboard(){
+        val imm = context?.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(titleView.windowToken, 0)
     }
 
 }
